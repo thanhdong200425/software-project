1d1896bb3de04905e593ef900d7c92f65d79af76
Thanh Dong <dongnt.22ite@vku.udn.vn>
2024-10-11 20:16:39 +0700

    add gunicorn and fix procfile file

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,4 +11,5 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT assigned by Heroku
+    app.run(host="0.0.0.0", port=port)
