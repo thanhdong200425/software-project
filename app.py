b3868b17ba1a77c63d4a66330b662f21f8618fad
Thanh Dong <dongnt.22ite@vku.udn.vn>
2024-11-12 17:53:32 +0700

    Refactor login page layout and styles; update database connection to hotel1.db, fix logic code in login and registry route

--- app.py
+++ app.py
@@ -1,96 +1,171 @@
 import os
 from datetime import datetime
-from flask import Flask, render_template, request, flash, redirect,url_for, session
-from database.database_function import add_new_record
+from typing import final
+
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from database.database_function import add_new_record, fetch_data_from_table
 from database.database_function import get_all_rooms
 from database.database_function import get_db_connection
 from helpers.validate import is_existing_data
 from functools import wraps
-import sqlite3
 import hashlib
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-connection = sqlite3.connect("hotel1.db", timeout=10)
-cursor = connection.cursor()
-connection.commit()
-connection.close()
 
 def login_required(f):
     @wraps(f)
     def check(*args, **kwargs):
-        if not session.get('loggedin'):
+        if not session.get("logged_in"):
             return redirect(url_for("login", next=request.url))
         return f(*args, **kwargs)
+
     return check
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    message = ''
-    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        email = request.form['email']
-        password = request.form['password']
-        
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()
-        
-        with sqlite3.connect("hotel1.db") as conn:
-            conn.execute("PRAGMA journal_mode=WAL;")
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM user WHERE email = ? AND password = ?", (email, hashed_password))
-            user = cursor.fetchone()
 
-        if user:
-            session['loggedin'] = True
-            session['userid'] = user[0]
-            session['name'] = user[1]
-            session['email'] = user[2]
-            
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        user = fetch_data_from_table(table="user", column="*", condition="email = ?",
+                                     condition_params=(email,), fetch_one=True)
+        
+
+        if user and user[3] == hashed_password:
+            session["logged_in"] = True
+            session["userid"] = user[0]
+
             if "next" in request.args:
-                return redirect(request.args['next'])
-                
+                return redirect(request.args["next"])
+
             return redirect("/")
         else:
-            mesage = 'Please enter correct email / password!'
-    return render_template('login.html')
+            message = "Please enter correct email / password!"
+            return render_template("login.html", message=message)
+    
+    return render_template("login.html")
 
-@app.route('/logout')
+
+@app.route("/logout")
+@login_required
 def logout():
     session.clear()
-    return redirect(url_for('login'))
-app.secret_key = os.urandom(24)
+    return redirect(url_for("login"))
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    message = ''
-    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form:
-        userName = request.form['name']
-        password = hashlib.sha256(request.form['password'].encode()).hexdigest()
-        email = request.form['email']
-        
-        with sqlite3.connect("hotel1.db") as conn:
-            conn.execute("PRAGMA journal_mode=WAL;")
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM user WHERE email = ?", (email,))
-            account = cursor.fetchone()
+    message = ""
+    if request.method == "POST":
+        username = request.form["name"]
+        password = request.form['password']
+        email = request.form["email"]
 
-            if account:
-                mesage = 'Account already exists!'
-            elif not userName or not password or not email:
-                mesage = 'Please fill out the form!'
-            else:
-                cursor.execute("INSERT INTO user (name, email, password) VALUES (?, ?, ?)", (userName, email, password))
-                conn.commit()
-                mesage = 'You have successfully registered!'
-                return render_template('login.html')
-    elif request.method == 'POST':
-        mesage = 'Please fill out the form!'
-    return render_template('register.html')
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        user = fetch_data_from_table(table="user", column="*", condition="email = ? AND password = ?",
+                                     condition_params=(email, hashed_password), fetch_one=True)
+        if user:
+                message = "Account already exists!"
+                return render_template("register.html", message=message)
+        elif not username or not password or not email:
+                message = "Please fill out the form!"
+                return render_template("register.html", message=message)
+        else:
+            # Add a new user into database
+            add_new_record(table="user", column=["name", "email", "password"], params=(username, email, hashed_password))
+            message = "You have successfully registered!"
+            return render_template("login.html", message=message)
+
+    return render_template("register.html")
 
 @app.route("/")
 @login_required
 def index():
-    return render_template("index.html")
+    # Just some mock data
+    bookings = [
+        {
+            "id": 1,
+            "customer_name": "John Doe",
+            "status": "Checked-in",
+            "book_day": "2024-10-15",
+            "expected_checkin": "2024-10-20",
+            "expected_checkout": "2024-10-25",
+            "actual_checkin": "2024-10-20",
+            "checkout": None,
+            "total_people": 2,
+        },
+        {
+            "id": 2,
+            "customer_name": "Jane Smith",
+            "status": "Checked-out",
+            "book_day": "2024-10-10",
+            "expected_checkin": "2024-10-18",
+            "expected_checkout": "2024-10-20",
+            "actual_checkin": "2024-10-18",
+            "checkout": "2024-10-20",
+            "total_people": 1,
+        },
+        {
+            "id": 3,
+            "customer_name": "Alice Johnson",
+            "status": "Checked-in",
+            "book_day": "2024-10-12",
+            "expected_checkin": "2024-10-21",
+            "expected_checkout": "2024-10-28",
+            "actual_checkin": "2024-10-21",
+            "checkout": None,
+            "total_people": 3,
+        },
+        {
+            "id": 4,
+            "customer_name": "Bob Brown",
+            "status": "Reserved",
+            "book_day": "2024-10-14",
+            "expected_checkin": "2024-10-22",
+            "expected_checkout": "2024-10-25",
+            "actual_checkin": None,
+            "checkout": None,
+            "total_people": 2,
+        },
+        {
+            "id": 5,
+            "customer_name": "Emily White",
+            "status": "Cancelled",
+            "book_day": "2024-10-17",
+            "expected_checkin": "2024-10-24",
+            "expected_checkout": "2024-10-27",
+            "actual_checkin": None,
+            "checkout": None,
+            "total_people": 0,
+        },
+        {
+            "id": 5,
+            "customer_name": "Emily White",
+            "status": "Cancelled",
+            "book_day": "2024-10-17",
+            "expected_checkin": "2024-10-24",
+            "expected_checkout": "2024-10-27",
+            "actual_checkin": None,
+            "checkout": None,
+            "total_people": 0,
+        },
+        # Add more bookings here as needed
+    ]
+    page = request.args.get("page", 1, type=int)
+    per_page = 5
+    total_pages = (len(bookings) - 1) // per_page + 1
+
+    # Get only the bookings for the current page
+    start = (page - 1) * per_page
+    end = start + per_page
+    paginated_bookings = bookings[start:end]
+
+    return render_template(
+        "index.html", bookings=paginated_bookings, page=page, total_pages=total_pages
+    )
+
 
 @app.route("/booking", methods=["GET", "POST"])
 @login_required
@@ -101,13 +176,18 @@ def booking():
     customer_name = request.form.get("customer-name")
     customer = is_existing_data("customer", "name", customer_name)
     if not customer:
-        flash("Username does not exist. Please try to enter another customer name", "error")
+        flash(
+            "Username does not exist. Please try to enter another customer name",
+            "error",
+        )
         return redirect("/booking")
 
     room_name = request.form.get("room")
     room = is_existing_data("room", "name", room_name)
     if not room:
-        flash("Room name does not exist. Please try to enter another room name", "error")
+        flash(
+            "Room name does not exist. Please try to enter another room name", "error"
+        )
         return redirect("/booking")
 
     checkout_date = request.form.get("check-out-date")
@@ -115,11 +195,28 @@ def booking():
     total_people = request.form.get("total-people")
     add_new_record(
         "booking",
-        ["customer_id", "room_id", "status", "book_day", "expected_checkin", "expected_checkout", "total_people"],
-        (customer[0], room[0], "ongoing", datetime.now(), checkin_date, checkout_date, total_people)
+        [
+            "customer_id",
+            "room_id",
+            "status",
+            "book_day",
+            "expected_checkin",
+            "expected_checkout",
+            "total_people",
+        ],
+        (
+            customer[0],
+            room[0],
+            "ongoing",
+            datetime.now(),
+            checkin_date,
+            checkout_date,
+            total_people,
+        ),
     )
     flash("Added booking", "success")
     return redirect("/booking")
+
 
 @app.route("/list_room", methods=["GET"])
 @login_required
@@ -128,73 +225,90 @@ def list_room():
     return render_template("list_room.html", rooms=rooms)
 
 
-
-
-
-@app.route('/rooms')
+@app.route("/rooms")
+@login_required
 def list_rooms():
     conn = get_db_connection()
-    rooms = conn.execute('SELECT * FROM room').fetchall()
+    rooms = conn.execute("SELECT * FROM room").fetchall()
     conn.close()
-    return render_template('list_room.html', rooms=rooms)
+    return render_template("list_room.html", rooms=rooms)
 
-@app.route('/list_room/create', methods=['GET', 'POST'])
+
+@app.route("/list_room/create", methods=["GET", "POST"])
+@login_required
 def create_room():
-    if request.method == 'POST':
-        room_name = request.form['name']
-        room_type = request.form['type']
-        price = request.form['price_per_night']
-        service = request.form['service']
-        description = request.form['description']
-        capacity = request.form['capacity']
-        status = request.form['status']
+    if request.method == "POST":
+        room_name = request.form["name"]
+        room_type = request.form["type"]
+        price = request.form["price_per_night"]
+        service = request.form["service"]
+        description = request.form["description"]
+        capacity = request.form["capacity"]
+        status = request.form["status"]
 
         conn = get_db_connection()
-        conn.execute('INSERT INTO room (name, type, price_per_night, service, description, capacity, status) VALUES (?, ?, ?, ?, ?, ?, ?)',
-                     (room_name, room_type, price, service, description, capacity, status))
+        conn.execute(
+            "INSERT INTO room (name, type, price_per_night, service, description, capacity, status) VALUES (?, ?, ?, ?, ?, ?, ?)",
+            (room_name, room_type, price, service, description, capacity, status),
+        )
         conn.commit()
         conn.close()
-        flash('Room added successfully!')
-        return redirect(url_for('list_rooms'))
+        flash("Room added successfully!")
+        return redirect(url_for("list_rooms"))
 
-    return render_template('create_room.html')
+    return render_template("create_room.html")
 
-@app.route('/list_room/edit/<int:room_id>', methods=['GET', 'POST'])
+
+@app.route("/list_room/edit/<int:room_id>", methods=["GET", "POST"])
+@login_required
 def edit_room(room_id):
     conn = get_db_connection()
-    room = conn.execute('SELECT * FROM room WHERE id = ?', (room_id,)).fetchone()
+    room = conn.execute("SELECT * FROM room WHERE id = ?", (room_id,)).fetchone()
 
-    if request.method == 'POST':
-        room_name = request.form['name']
-        room_type = request.form['type']
-        price = request.form['price_per_night']
-        service = request.form['service']
-        description = request.form['description']
-        capacity = request.form['capacity']
-        status = request.form['status']
+    if request.method == "POST":
+        room_name = request.form["name"]
+        room_type = request.form["type"]
+        price = request.form["price_per_night"]
+        service = request.form["service"]
+        description = request.form["description"]
+        capacity = request.form["capacity"]
+        status = request.form["status"]
 
-        if status not in ['available', 'ongoing', 'closed']:
-            flash('Invalid status value.', 'error')
-            return redirect(url_for('edit_room', room_id=room_id))
+        if status not in ["available", "ongoing", "closed"]:
+            flash("Invalid status value.", "error")
+            return redirect(url_for("edit_room", room_id=room_id))
 
-        conn.execute('UPDATE room SET name = ?, type = ?, price_per_night = ?, service = ?, description = ?, capacity = ?, status = ? WHERE id = ?',
-                     (room_name, room_type, price, service, description, capacity, status, room_id))
+        conn.execute(
+            "UPDATE room SET name = ?, type = ?, price_per_night = ?, service = ?, description = ?, capacity = ?, status = ? WHERE id = ?",
+            (
+                room_name,
+                room_type,
+                price,
+                service,
+                description,
+                capacity,
+                status,
+                room_id,
+            ),
+        )
         conn.commit()
         conn.close()
-        flash('Room updated successfully!')
-        return redirect(url_for('list_rooms'))
+        flash("Room updated successfully!")
+        return redirect(url_for("list_rooms"))
 
     conn.close()
-    return render_template('edit_room.html', room=room)
+    return render_template("edit_room.html", room=room)
 
-@app.route('/rooms/delete/<int:room_id>', methods=['POST'])
+
+@app.route("/rooms/delete/<int:room_id>", methods=["POST"])
+@login_required
 def delete_room(room_id):
     conn = get_db_connection()
-    conn.execute('DELETE FROM room WHERE id = ?', (room_id,))
+    conn.execute("DELETE FROM room WHERE id = ?", (room_id,))
     conn.commit()
     conn.close()
-    flash('Room deleted successfully!')
-    return redirect(url_for('list_rooms'))
+    flash("Room deleted successfully!")
+    return redirect(url_for("list_rooms"))
 
 
 if __name__ == "__main__":
--- database/database_function.py
+++ database/database_function.py
@@ -2,14 +2,19 @@ import sqlite3
 import hashlib
 
 def get_db_connection():
-    conn = sqlite3.connect('hotel.db')
+    conn = sqlite3.connect('hotel1.db')
     conn.row_factory = sqlite3.Row
     return conn
 
 def fetch_data_from_table(table, column, condition=None, condition_params=(), fetch_one=False):
-    connection = sqlite3.connect('hotel.db')
+    connection = sqlite3.connect('hotel1.db')
     cursor = connection.cursor()
-    column_str = ", ".join(column)
+
+    if column != "*":
+        column_str = ", ".join(column)
+    else:
+        column_str = column
+
     if condition:
         query = f"SELECT {column_str} FROM {table} WHERE {condition}"
         cursor.execute(query, condition_params)
@@ -23,7 +28,7 @@ def fetch_data_from_table(table, column, condition=None, condition_params=(), fe
 
 
 def add_new_record(table, column, params=()):
-    connection = sqlite3.connect('hotel.db')
+    connection = sqlite3.connect('hotel1.db')
     cursor = connection.cursor()
     column_str = ", ".join(column)
     placeholders = ", ".join(["?"] * len(params))
@@ -34,7 +39,7 @@ def add_new_record(table, column, params=()):
 
 
 def get_all_rooms():
-    connection = sqlite3.connect('hotel.db')
+    connection = sqlite3.connect('hotel1.db')
     cursor = connection.cursor()
     query = "SELECT * FROM room"
     cursor.execute(query)
@@ -44,7 +49,7 @@ def get_all_rooms():
     return rooms
 
 def read_users():
-    connection = sqlite3.connect('hotel.db')
+    connection = sqlite3.connect('hotel1.db')
     cursor = connection.cursor()
     query = "SELECT * FROM user"
     cursor.execute(query)
@@ -53,7 +58,7 @@ def read_users():
     return users
 
 def validate_user(email, password):
-    connection = sqlite3.connect('hotel.db')
+    connection = sqlite3.connect('hotel1.db')
     cursor = connection.cursor()
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     query = "SELECT * FROM user WHERE email = ? AND password = ?"
