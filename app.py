a45cdb7d0c871edf34e6312327328303d0f7f3bc
Thanh Dong <dongnt.22ite@vku.udn.vn>
2024-11-16 08:36:14 +0700

    .

--- app.py
+++ app.py
@@ -315,4 +315,4 @@ def delete_room(room_id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
